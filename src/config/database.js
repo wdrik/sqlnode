fd34f7b00a15b4f97fe00e5e09ce1e7d3058370c
Iorgen Wildrik <w.drik@outlook.com>
2019-12-17 21:51:56 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  host: "127.0.0.1",
+  username: "root",
+  password: "root",
+  database: "sqlnode",
+  dialect: "mysql",
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+};
